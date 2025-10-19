cb09056acd89c61b4f4e307448726b6009af965e
Ben10-data <mohamed.benomar@ucad.edu.sn>
2025-10-19 23:25:32 +0000

    petite modification de notre  script

--- execution_hote.sh
+++ execution_hote.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 fichier=$1 
-echo "Hi, Mr ---------Ben----------------- "
-echo "On entamme l'etape de l'execution de notre code"
-
 # Ici on a juste copier la commande docker pour allumer un container et executer son code.
+# a rapeler le fichier que ce script va prendre en argument doit exister dans le app/
 
 docker exec -it spark-master   spark-submit --master spark://spark-master:7077 /opt/spark/app/$fichier
-
 
